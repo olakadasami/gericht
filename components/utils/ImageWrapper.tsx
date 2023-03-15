cc7e6177576fc9ce275734e6fc2dda41a15cd1c5
Samuel Olakada <olakadasamuel@gmail.com>
2023-03-15 08:47:44 +0100

    feat: created image wrapper with side borders

--- components/utils/ImageWrapper.tsx
+++ components/utils/ImageWrapper.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 
-const ImageWrapper = ({ imageUrl }: { imageUrl: string }) => {
+const ImageWrapper = ({ imageUrl, alt }: { imageUrl: StaticImageData, alt: string }) => {
     return (
-        <div className="relative w-1/2 border">
-            <Image src={imageUrl} fill alt="" />
+        <div
+            className="relative w-full h-full z-[10]
+            before:absolute before:h-2/3 before:w-2/3 before:top-[-3%] before:right-[-3%] before:bg-color-accent 
+            after:absolute after:h-2/3 after:w-2/3 after:bottom-[-3%] after:z-[-1] after:left-[-3%] after:bg-color-accent"
+        >
+            <Image src={imageUrl} fill alt={alt} />
         </div>
     )
 }
