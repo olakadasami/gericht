f2c385e41c74bc367e74dfee3dd5b92f7f72efe3
Samuel Olakada <olakadasamuel@gmail.com>
2023-03-15 09:02:32 +0100

    chore: created AboutItem component

--- components/Sections/About/About.tsx
+++ components/Sections/About/About.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
 import bgImage from '../../../assets/bg-about.png'
-import spoon from '../../../assets/spoon.png'
-import Button from '../../Button/Button'
+import AboutItem from './AboutItem'
 
 const About = () => {
     const style = {
@@ -33,33 +31,3 @@ const About = () => {
 }
 
 export default About
-
-interface Props {
-    heading: string
-    paragraph: string
-    right?: boolean
-}
-
-const AboutItem = ({ heading, paragraph, right }: Props) => {
-    return (
-        <div className={`${right ? 'items-end' : 'items-start'} flex flex-col`}>
-            <h2 className='text-color-accent text-3xl comorant'>{heading}</h2>
-            <div className='w-10 h-[9px] relative'>
-                <Image
-                    src={spoon}
-                    fill
-                    alt={heading}
-                />
-            </div>
-            <p className={`${right ? 'text-right' : ''} my-4`}>
-                {paragraph}
-            </p>
-            <div>
-                <Button
-                    href={'/'}
-                    title={'Know More'}
-                />
-            </div>
-        </div>
-    )
-}
